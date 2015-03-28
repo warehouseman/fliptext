f0c95d190f1d25b9b22efd8ae81a402050b2ccd3
Martin 'Hasan' Bramwell <martinhbramwell@gmail.com>
2015-03-28 07:08:01 -0400

    trying to debug Travis failure

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'warehouseman:fliptext',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Test the javascript function "flipext"',
   // URL to the Git repository containing the source code for this package.
@@ -12,7 +12,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.4.2');
-  api.use('jquery', 'client');
+//  api.use('jquery', 'client');
 
   api.addFiles('fliptext.js');
 
